58155b5b3a50693512016f20d0d519b35eb6a231
Renato <re2005@gmail.com>
2020-08-01 13:53:26 +0200

    feat: add docker support

--- message.js
+++ message.js
@@ -2,15 +2,24 @@ const exec = require('child_process').exec;
 const WebSocket = require('ws');
 const fetch = require('node-fetch');
 
-const wsUri = 'ws://localhost:8280/';
+const wsUri = 'ws://host.docker.internal:8280/';
 const websocket = new WebSocket(wsUri);
 websocket.onopen = () => console.log('OPEN');
 websocket.onclose = () => console.log('CLOSED');
 websocket.onerror = (e) => console.log('ERROR', e);
-websocket.onmessage = (m) => onMessage(m);
 
-const config = process.argv.slice(2);
-console.log('Bot is set for user:', config[0]);
+const config =  ['mober@peer2peer.ai'];
+let userId;
+
+exec('bitdust id', function (error, response) {
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+    userId = response;
+    console.log('Bot is set for user:' + userId);
+    websocket.onmessage = (m) => onMessage(m);
+});
+
 
 /**
  *
@@ -34,7 +43,8 @@ async function getJoke() {
  */
 function sendMessage(sender, message) {
     message = message.replace(/'/g, '').replace(/Chuck Norris/g, sender.replace(/@.*$/, '').toUpperCase());
-    const command = `curl -X POST -d '{"global_id":"` + sender + `","data":{"type":"text", "message":` + message + `}}' localhost:8180/message/send/v1`;
+    const command = `curl -X POST -d '{"global_id":"` + sender + `","data":{"type":"text", "message":"` + message + `"}}' host.docker.internal:8180/message/send/v1`;
+    console.log(command);
     exec(command, function (error) {
         if (error !== null) {
             console.log('exec error: ' + error);
@@ -49,9 +59,9 @@ function sendMessage(sender, message) {
  */
 async function onMessage(m) {
     const data = JSON.parse(m.data);
-    if (data.type === 'private_message') {
+    if (data.type === 'stream_message') {
         const sender = data.payload.sender.glob_id.replace('master$', '');
-        const authorIsMe = config[0] === sender;
+        const authorIsMe = userId === sender;
         if (!authorIsMe) {
             const joke = await getJoke();
             sendMessage(sender, joke);
